7bc1a6d84b5f9ca7f094d3294453032471e87d48
Aaditya Sharma <aadityasharma00445@gmail.com>
2025-02-26 23:09:41 +0530

    Added conf and components

--- blog-nest/src/App.jsx
+++ blog-nest/src/App.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react'
+import { useState , useEffect } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import Header from './components/header/Header'
+import Footer from './components/Footer/Footer'
+import Outlet from 'react-router-dom'
 
 
 function App() {
-    console.log();
+    const [loading, setloading] = useState(true)
+    const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>A blog app</h1>
-    </>
-  )
+    useEffect(() => {
+      authService.getCurrentUser()
+      .then((userData) => {
+        if(userData){
+          dispatch(login({userData}))
+        }else{
+          dispatch(logout())
+        }
+      })
+      .finally(() => setloading(false))
+    }, [])
+    
+
+    return !loading ? (
+      <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header/>
+          <main>
+            {/* <Outlet/> */}
+          </main>
+          <Footer/>
+        </div>
+      </div>
+    ) : null
 }
 
 export default App
---
+++ blog-nest/src/components/index.js
@@ -0,0 +1,11 @@
+import Header from "./header/Header";
+import Footer from "./Footer/Footer";
+import container from "./container/container";
+
+
+export { 
+    Header,
+    Footer,
+    container,
+    
+}
